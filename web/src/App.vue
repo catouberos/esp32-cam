4fc7dd4607896c219eb21c8665b9902c0e6fdfec
Nguyen Do <khoanguyen.do@outlook.com>
2024-12-06 03:32:13 +0700

    feat: add static web

---
+++ web/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppCamera from "./AppCamera.vue";
+</script>
+
+<template>
+  <main class="container mx-auto p-6 space-y-6">
+    <AppCamera />
+  </main>
+</template>

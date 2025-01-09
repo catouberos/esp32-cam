cc5c3567f7d6e0836df68fa2ce8c96ecceca4d08
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-10 03:00:56 +0700

    build: add manual mode

--- web/src/components/NumpadControl.vue
+++ web/src/components/NumpadControl.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import GamepadButton from "./GamepadButton.vue";
+
+const selected = ref(10);
 
 defineEmits<{
   click: [number];
@@ -9,9 +12,28 @@ defineEmits<{
 <template>
   <div class="space-x-3">
     <template v-for="i in 9">
-      <GamepadButton @pointerdown="() => $emit('click', i)">
+      <GamepadButton
+        :class="{ 'font-bold': selected === i }"
+        @pointerdown="
+          () => {
+            selected = i;
+            $emit('click', i);
+          }
+        "
+      >
         {{ i }}
       </GamepadButton>
     </template>
+    <GamepadButton
+      :class="{ 'font-bold': selected === 10 }"
+      @pointerdown="
+        () => {
+          selected = 10;
+          $emit('click', 10);
+        }
+      "
+    >
+      M
+    </GamepadButton>
   </div>
 </template>
